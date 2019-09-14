da0603bbae9803554aad63a62ec328f93d9e2489
jordanonuma <jordanonuma@outlook.com>
2019-09-14 11:48:22 -1000

    Added Extra Credit.

--- ex4.py
+++ ex4.py
@@ -13,3 +13,15 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpool today."
 print "We need to put about", average_passengers_per_car, "in each car." #this probably rounds to nearest integer (no floating point)
+
+#Extra Credit
+# Traceback (most recent call last):
+# File "ex4.py", line 8, in <module>
+# average_passengers_per_car = car_pool_capacity / passenger
+# NameError: name 'car_pool_capacity' is not defined
+
+# Reason: 'car_pool_capacity is undefined wherease 'carpool_capacity' is
+
+# Remainder of Extra Credit:
+# 1. Carpool_capacity gives '120.0' because '4.0' is given for space per car.
+# 2. Floating point numbers are simply numbers with a decimal.
