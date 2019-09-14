ac0bdde8014b58b285c1a702725d8928ed5a975f
jordanonuma <jordanonuma@outlook.com>
2019-09-14 11:35:51 -1000

    Added remainder printouts.

--- ex4.py
+++ ex4.py
@@ -7,4 +7,9 @@ cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
-print "There are", cars, "cars available."
+print "There are", cars, "cars available." #no extra spaces in "" necessary
+print "There are only", drivers, "drivers available." #less drivers than cars
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car." #this probably rounds to nearest integer (no floating point)
