68b8c76f7de47f699c5cc67e11c09620c8f123b1
jordanonuma <jordanonuma@outlook.com>
2019-09-14 11:30:16 -1000

    Set variables for car example.

---
+++ ex4.py
@@ -0,0 +1,7 @@
+cars = 100
+space_in_a_car = 30
+drivers = 30
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
