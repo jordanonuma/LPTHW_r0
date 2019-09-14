721b347806ab04825ab542a91e551eca9c95caa2
jordanonuma <jordanonuma@outlook.com>
2019-09-14 11:14:25 -1000

    Added relational operator example.

--- ex3.py
+++ ex3.py
@@ -6,3 +6,10 @@ print "Roosters", 100 - 25 * 3 % 4 #guess: 100 - 75 % 4 = 100 - 3 = 97
 print "Now I will count the eggs:"
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #guess: 1 + 0 - 0.25 + 6 = 6.75
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
