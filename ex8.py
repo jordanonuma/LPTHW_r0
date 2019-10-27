683d0aacc869e709a9f6bc36c8378e24444c2af4
jordanonuma <jordanonuma@outlook.com>
2019-10-27 12:47:17 -1000

    Adds the string version of variable 'formatter' in single quotes.

--- ex8.py
+++ ex8.py
@@ -3,4 +3,4 @@ formatter = "%r %r %r %r"
 print formatter % (1, 2, 3, 4) #outputs "1 2 3 4"
 print formatter % ("one", "two", "three", "four") #outputs "'one' 'two' 'three' 'four'"
 print formatter % (True, False, False, True) #outputs "True False False True"
-print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (formatter, formatter, formatter, formatter) #outputs "'%r %r %r %r' '%r %r %r %r' '%r %r %r %r' '%r %r %r %r'"
