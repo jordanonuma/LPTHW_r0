54c45d3877a80b811892f154ed43bade19e479a3
jordanonuma <jordanonuma@outlook.com>
2019-10-27 12:44:21 -1000

    Adding just one string of text in parentheses gives an error. Need one string for each %r.

--- ex8.py
+++ ex8.py
@@ -3,3 +3,4 @@ formatter = "%r %r %r %r"
 print formatter % (1, 2, 3, 4) #outputs "1 2 3 4"
 print formatter % ("one", "two", "three", "four") #outputs "'one' 'two' 'three' 'four'"
 print formatter % (True, False, False, True) #outputs "True False False True"
+printer formatter % (formatter)
