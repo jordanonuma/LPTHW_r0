fbe6dff6973f0a329250cf22eb9fde8639eaa2cc
jordanonuma <jordanonuma@outlook.com>
2019-10-26 17:22:06 -1000

    Added variables.

--- ex7.py
+++ ex7.py
@@ -2,3 +2,16 @@ print "Mary had a little lamb."
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
 print "." * 10 #this prints the period "." 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r" 
