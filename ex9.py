e3dc14b7a63ba4e087530e4d02296bd848ed9cf7
jordanonuma <jordanonuma@outlook.com>
2019-11-02 17:26:04 -1000

    Added output. '\n' creates line indent.

--- ex9.py
+++ ex9.py
@@ -4,4 +4,11 @@ days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJuly\nAug"
 
 print "Here are the days: ", days #Outputs: "Mon Tue Wed thu Fri Sat Sun"
-print "Here are the months: ", months
+print "Here are the months: ", months #Outputs: Jan
+#Feb
+#Mar
+#Apr
+#May
+#Jun
+#Jul
+#Aug
