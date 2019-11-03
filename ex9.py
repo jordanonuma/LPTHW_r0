a073b7685d2eb437323f994f8eecccb5fb1a8710
jordanonuma <jordanonuma@outlook.com>
2019-11-02 17:24:17 -1000

    Added basic print of days of the week.

---
+++ ex9.py
@@ -0,0 +1,6 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJuly\nAug"
+
+print "Here are the days: ", days #Outputs: "Mon Tue Wed thu Fri Sat Sun"
