657a03355cd7e02a2620be7dd0798fb7ef9aceae
jordanonuma <jordanonuma@outlook.com>
2019-11-02 17:27:12 -1000

    Added section with three sets of double quotes.

--- ex9.py
+++ ex9.py
@@ -12,3 +12,10 @@ print "Here are the months: ", months #Outputs: Jan
 #Jun
 #Jul
 #Aug
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""""
