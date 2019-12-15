a9fb66f5d3d285ecad9a798fcea8187f347a2b63
jordanonuma <jordanonuma@outlook.com>
2019-12-14 15:04:28 -1000

    Return user's age, height, and weight.

--- ex11.py
+++ ex11.py
@@ -4,3 +4,7 @@ print "How tall are you (inches)?",
 height = raw_input()
 print "How much do you weigh (lbs)?",
 weight = raw_input()
+
+print "Hello, you're %r old, %r inches tall, and %r lbs heavy." % (
+    age, height, weight
+)
