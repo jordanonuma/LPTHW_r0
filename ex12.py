25539600b624057e4ddc19696800649e0d1a8cb7
jordanonuma <jordanonuma@outlook.com>
2019-12-29 21:33:19 -1000

    Added printed return for user's age, height, and weight.

--- ex12.py
+++ ex12.py
@@ -1,3 +1,5 @@
-age = raw_input("How old are you? ")
+age = raw_input("How many years old are you? ")
 height = raw_input("How tall are you [in]?")
 weight = raw_input("How heavy are you [lbs]?")
+
+print "I gather you are %r years old, %r inches tall, and %r lbs heavy." % (age, height, weight)
