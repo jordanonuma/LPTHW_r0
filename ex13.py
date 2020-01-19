8691bcb0bc10d7a0ac4dba4b63db57102dac71ec
jordanonuma <jordanonuma@outlook.com>
2020-01-18 18:53:57 -1000

    Commented out lines with additional argument variables.

--- ex13.py
+++ ex13.py
@@ -1,9 +1,11 @@
-from sys import argv, raw_input #'sys' module with 'argv' variable passed to the python script.
+from sys import argv #'sys' module with 'argv' variable passed to the python script.
 
 script, first, second, third = argv
-fourth, fifth = raw_input
+#fourth, fifth = raw_input
 
 print "The script is called:", script
 print "The first variable is:", first
 print "The second variable is:", second
 print "The third variable is:", third
+#print "The fourth variable is:", fourth
+#print "The fifth variable is:", fifth
