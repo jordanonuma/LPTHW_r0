fed4a61a3de67fd30b8c29a2e6961a411d43b10f
jordanonuma <jordanonuma@outlook.com>
2020-01-18 18:47:14 -1000

    Printed each of the variables within 'argv'.

--- ex13.py
+++ ex13.py
@@ -1,3 +1,8 @@
 from sys import argv #'sys' module with 'argv' variable passed to the python script.
 
 script, first, second, third = argv
+
+print "The script is called:", script
+print "The first variable is:", first
+print "The second variable is:", second
+print "The third variable is:", third
