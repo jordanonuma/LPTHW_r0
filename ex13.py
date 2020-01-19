dd940ad7b918ee7cf75749ebd500f07f77fb3514
jordanonuma <jordanonuma@outlook.com>
2020-01-18 18:50:21 -1000

    Attempting to add additional argument variable.

--- ex13.py
+++ ex13.py
@@ -1,6 +1,7 @@
-from sys import argv #'sys' module with 'argv' variable passed to the python script.
+from sys import argv, raw_input #'sys' module with 'argv' variable passed to the python script.
 
 script, first, second, third = argv
+fourth, fifth = raw_input
 
 print "The script is called:", script
 print "The first variable is:", first
