8b6876e4cfa1cc60b66244befbf677ee658e6400
jordanonuma <jordanonuma@outlook.com>
2020-01-25 20:13:13 -1000

    Added prompts.

--- ex14.py
+++ ex14.py
@@ -4,3 +4,5 @@ script, user_name = argv #run using 'python ex14.py Tanjiro"
 prompt = '> '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
+print "I'd like to ask you a a few questions."
+print "Do you like me %s?" % user_name
