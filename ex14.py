40cad80da6cde587913b91fc746ffe04a306dfc2
jordanonuma <jordanonuma@outlook.com>
2020-01-25 20:14:10 -1000

    Added prompt to user's location.

--- ex14.py
+++ ex14.py
@@ -6,3 +6,7 @@ prompt = '> '
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a a few questions."
 print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
