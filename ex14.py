0b7e849bc866eb78ee6b750654ea9fa6a4e70608
jordanonuma <jordanonuma@outlook.com>
2020-01-25 20:12:12 -1000

    Revised syntax/spelling.

--- ex14.py
+++ ex14.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-script, user_name = argv
+script, user_name = argv #run using 'python ex14.py Tanjiro"
 prompt = '> '
 
-print "Hi %s, I'm the % script." % (user_name, script)
+print "Hi %s, I'm the %s script." % (user_name, script)
