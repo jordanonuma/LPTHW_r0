a3d8e6e137fa3d81242902272e3ff2fcc9645e5c
jordanonuma <jordanonuma@outlook.com>
2020-03-14 21:49:52 -1000

    Added lines 2 and 3 that user provides.

--- ex16.py
+++ ex16.py
@@ -23,3 +23,8 @@ line3 = raw_input("line 3: ")
 print "I'm going to write these to the file."
 
 target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
