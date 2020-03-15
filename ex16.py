c2c33ce04cb8485b14273c75ff15ee5ddb485c2a
jordanonuma <jordanonuma@outlook.com>
2020-03-14 21:42:59 -1000

    Prompts the user for their response, and if continuing, opens the file.

--- ex16.py
+++ ex16.py
@@ -5,3 +5,8 @@ script, filename = argv
 print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
